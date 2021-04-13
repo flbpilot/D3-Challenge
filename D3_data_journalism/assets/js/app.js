3ff2c596c2e42c6a87fa3845e3b36b130fce9d8d
Flbpilot <68716202+flbpilot@users.noreply.github.com>
2021-04-13 16:15:02 -0400

    4-13-2021_4:15PMEST

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -14,7 +14,6 @@ var width = svgWidth - margin.left - margin.right;
 var height = svgHeight - margin.top - margin.bottom;
 
 //SVG wrapper and hold it to top left of the screen
-
 var svg = d3.select("#scatter")
   .append("svg")
   .attr("width", svgWidth)
@@ -24,7 +23,7 @@ var chartGroup = svg.append("g")
   .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
 //import data
-d3.csv("/assets/data/data.csv")
+d3.csv("assets/data/data.csv")
 .then(function(censusData) {
   console.log(censusData)
   censusData.forEach(function(data) {
@@ -37,3 +36,73 @@ d3.csv("/assets/data/data.csv")
 });
 
 //Scale Function
+var xLinearScale = d3.scaleLinear()
+.domain([6, d3.max(censusData, d => d.healthcare)])
+.range([0, width]);
+
+var yLinearScale = d3.scaleLinear()
+.domain([4, d3.max(censusData, d => d.age)])
+.range([height, 0]);
+
+
+//Create and Append Axis
+var bottomAxis = d3.axisBottom(xLinearScale);
+var leftAxis = d3.axisLeft(yLinearScale);
+
+chartGroup.append("g")
+.attr("transform", `translate(0, ${height})`)
+.call(bottomAxis);
+
+chartGroup.append("g")
+.call(leftAxis);
+
+//Add Circles
+var circlesGroup = chartGroup.selectAll("circle")
+.data(censusData)
+.enter()
+.append("circle")
+.attr("cx", d => xLinearScale(d.healthcare))
+.attr("cy", d => yLinearScale(d.age))
+.attr("r", "15")
+.attr("fill", "#ECB55A");
+
+//Setup Tool Tip
+var toolTip = d3.tip()
+.attr("class", "tooltip")
+.offset([80, -60])
+.html(function(data, index) {
+  return (`${data.state}<br>Healthcare: ${data.healthcare} %<br>Age: ${data.age} %`);
+});
+
+    // Create tooltip in the chart
+    // ==============================
+    chartGroup.call(toolTip);
+
+    //  Create event listeners to display and hide the tooltip
+    // ==============================
+    circlesGroup.on("mouseover", function(data) {
+      toolTip.show(data, this);
+    })
+      // onmouseout event
+      .on("mouseout", function(data, index) {
+        toolTip.hide(data);
+      });
+
+    // Create axes labels
+    chartGroup.append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 0 - margin.left + 40)
+      .attr("x", 0 - (height / 2))
+      .attr("dy", "1em")
+      .attr("class", "axisText")
+      .text("Age Rate (%)");
+
+    chartGroup.append("text")
+      .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
+      .attr("class", "axisText")
+      .text("Healthcare Rate (%)");
+  }).catch(function(error) {
+    console.log(error);
+  });
+
+  
