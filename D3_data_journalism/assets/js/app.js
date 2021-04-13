de7acb9d15cbc8160c4c4ac15561428d71210aa1
Flbpilot <68716202+flbpilot@users.noreply.github.com>
2021-04-13 15:05:01 -0400

    4-13-2021_3:04PMEST

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -24,6 +24,26 @@ acsData.forEach(function(data) {
   data.obesity = +data.obesity;
   data.smokes = +data.smokes;
 });
-  
+});
 
 //SVG wrapper and hold it to top left of the screen
+var svg = d3.select("#scatter")
+  .append("svg")
+  .attr("width", svgWidth)
+  .attr("height", svgHeight);
+
+var chartGroup = svg.append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+// Import Data
+d3.csv("assets/data/data.csv").then(function(censusData) {
+
+  console.log(censusData)
+        // Step 1: Parse Data/Cast as numbers
+        censusData.forEach(function(data) {
+        data.poverty = +data.poverty;
+        data.smokes = +data.smokes;
+
+    });
+
+  });
