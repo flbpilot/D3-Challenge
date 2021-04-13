47b6da15c61af5caa744f2b9a8a35fea7d352b58
Flbpilot <68716202+flbpilot@users.noreply.github.com>
2021-04-13 14:57:25 -0400

    4-13-2021_2:57PMEST

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -1,3 +1,4 @@
+//SVG dimension and position
 
 var svgWidth = 800;
 var svgHeight = 500;
@@ -11,3 +12,18 @@ var margin = {
 
 var width = svgWidth - margin.left - margin.right;
 var height = svgHeight - margin.top - margin.bottom;
+
+//import data
+d3.csv("D3_data_journalism/assets/data/data.csv")
+.then(function(acsData) {
+acsData.forEach(function(data) {
+  data.poverty = +data.poverty;
+  data.age = +data.age;
+  data.income = +data.income;
+  data.healthcare = +data.healthcare;
+  data.obesity = +data.obesity;
+  data.smokes = +data.smokes;
+});
+  
+
+//SVG wrapper and hold it to top left of the screen
