ac0a534d5fefdfd8317feede08f5fbd93984cd96
Flbpilot <68716202+flbpilot@users.noreply.github.com>
2021-04-13 15:57:49 -0400

    4-13-2021_3:57PMEST

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -13,20 +13,8 @@ var margin = {
 var width = svgWidth - margin.left - margin.right;
 var height = svgHeight - margin.top - margin.bottom;
 
-//import data
-d3.csv("D3_data_journalism/assets/data/data.csv")
-.then(function(acsData) {
-acsData.forEach(function(data) {
-  data.poverty = +data.poverty;
-  data.age = +data.age;
-  data.income = +data.income;
-  data.healthcare = +data.healthcare;
-  data.obesity = +data.obesity;
-  data.smokes = +data.smokes;
-});
-});
-
 //SVG wrapper and hold it to top left of the screen
+
 var svg = d3.select("#scatter")
   .append("svg")
   .attr("width", svgWidth)
@@ -35,11 +23,17 @@ var svg = d3.select("#scatter")
 var chartGroup = svg.append("g")
   .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
+//import data
+d3.csv("/assets/data/data.csv")
+.then(function(censusData) {
   console.log(censusData)
-        // Step 1: Parse Data/Cast as numbers
-        censusData.forEach(function(data) {
-        data.poverty = +data.poverty;
-        data.smokes = +data.smokes;
+  censusData.forEach(function(data) {
+  data.poverty = +data.poverty;
+  data.age = +data.age;
+  data.income = +data.income;
+  data.healthcare = +data.healthcare;
+  data.obesity = +data.obesity;
+  data.smokes = +data.smokes;
+});
 
-    });
-
+//Scale Function
